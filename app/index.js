61b642afe5d95f463332fb30e0c8a87b8b6a0dd6
Jake Whetstone <jake@indrez.com>
2014-07-14 10:21:17 -0400

    Moving bootstrap.less into file for better readability in versioning

--- app/index.js
+++ app/index.js
@@ -64,12 +64,6 @@ var BootstrapEditVarsGenerator = yeoman.generators.Base.extend({
         this.write('app/styles/main.less', contents);
     },
 
-    bootstrapStylesheet: function () {
-        var html = '@path:"../bower_components/bootstrap/less/";\n\n// Core variables and mixins\n@import "variables.less";\n@import "@{path}mixins.less";\n\n// Reset\n@import "@{path}normalize.less";\n@import "@{path}print.less";\n\n// Core CSS\n@import "@{path}scaffolding.less";\n@import "@{path}type.less";\n@import "@{path}code.less";\n@import "@{path}grid.less";\n@import "@{path}tables.less";\n@import "@{path}forms.less";\n@import "@{path}buttons.less";\n\n// Components\n@import "@{path}component-animations.less";\n@import "@{path}glyphicons.less";\n@import "@{path}dropdowns.less";\n@import "@{path}button-groups.less";\n@import "@{path}input-groups.less";\n@import "@{path}navs.less";\n@import "@{path}navbar.less";\n@import "@{path}breadcrumbs.less";\n@import "@{path}pagination.less";\n@import "@{path}pager.less";\n@import "@{path}labels.less";\n@import "@{path}badges.less";\n@import "@{path}jumbotron.less";\n@import "@{path}thumbnails.less";\n@import "@{path}alerts.less";\n@import "@{path}progress-bars.less";\n@import "@{path}media.less";\n@import "@{path}list-group.less";\n@import "@{path}panels.less";\n@import "@{path}wells.less";\n@import "@{path}close.less";\n\n// Components w/ JavaScript\n@import "@{path}modals.less";\n@import "@{path}tooltip.less";\n@import "@{path}popovers.less";\n@import "@{path}carousel.less";\n\n// Utility classes\n@import "@{path}utilities.less";\n@import "@{path}responsive-utilities.less";';
-
-        this.write('app/styles/bootstrap.less', html);
-    },
-
     configuring: function () {
         this.dest.mkdir('app');
         this.dest.mkdir('app/scripts');
@@ -89,6 +83,7 @@ var BootstrapEditVarsGenerator = yeoman.generators.Base.extend({
         this.copy('robots.txt', 'app/robots.txt');
         this.copy('htaccess', 'app/.htaccess');
         this.copy('variables.less', 'app/styles/variables.less');
+		this.copy('bootstrap.less', 'app/styles/bootstrap.less');
         //Ideally we'd copy this from bootstrap3 after it's installed, but there doesn't seem to be a good mechanism for that right now.
         this.template('Gruntfile.js');
         this.template('_package.json', 'package.json');
